76f1060c9980f34ccdc84c87844bc21395c8e50f
borg <elm.tiago@gmail.com>
2020-04-30 15:53:03 -0300

    Class Terminator associada ao robô do gazebo

--- ros/scripts/base_proj.py
+++ ros/scripts/base_proj.py
@@ -31,6 +31,7 @@ from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image
 from std_msgs.msg import Header
+from TerminatorModule import Terminator
 
 import visao_module
 
@@ -38,98 +39,33 @@ import visao_module
 bridge = CvBridge()
 
 cv_image = None
-media = []
-centro = []
 atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
 
 
-area = 0.0 # Variavel com a area do maior contorno
-
-# Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. 
-# Descarta imagens que chegam atrasadas demais
-check_delay = False 
-
-resultados = [] # Criacao de uma variavel global para guardar os resultados vistos
-
-x = 0
-y = 0
-z = 0 
-id = 0
-
-frame = "camera_link"
-# frame = "head_camera"  # DESCOMENTE para usar com webcam USB via roslaunch tag_tracking usbcam
-
-tfl = 0
-
-tf_buffer = tf2_ros.Buffer()
-
-def recebe(msg):
-	global x # O global impede a recriacao de uma variavel local, para podermos usar o x global ja'  declarado
-	global y
-	global z
-	global id
-	for marker in msg.markers:
-		id = marker.id
-		marcador = "ar_marker_" + str(id)
-
-		print(tf_buffer.can_transform(frame, marcador, rospy.Time(0)))
-		header = Header(frame_id=marcador)
-		# Procura a transformacao em sistema de coordenadas entre a base do robo e o marcador numero 100
-		# Note que para seu projeto 1 voce nao vai precisar de nada que tem abaixo, a 
-		# Nao ser que queira levar angulos em conta
-		trans = tf_buffer.lookup_transform(frame, marcador, rospy.Time(0))
-		
-		# Separa as translacoes das rotacoes
-		x = trans.transform.translation.x
-		y = trans.transform.translation.y
-		z = trans.transform.translation.z
-		# ATENCAO: tudo o que vem a seguir e'  so para calcular um angulo
-		# Para medirmos o angulo entre marcador e robo vamos projetar o eixo Z do marcador (perpendicular) 
-		# no eixo X do robo (que e'  a direcao para a frente)
-		t = transformations.translation_matrix([x, y, z])
-		# Encontra as rotacoes e cria uma matriz de rotacao a partir dos quaternions
-		r = transformations.quaternion_matrix([trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w])
-		m = numpy.dot(r,t) # Criamos a matriz composta por translacoes e rotacoes
-		z_marker = [0,0,1,0] # Sao 4 coordenadas porque e'  um vetor em coordenadas homogeneas
-		v2 = numpy.dot(m, z_marker)
-		v2_n = v2[0:-1] # Descartamos a ultima posicao
-		n2 = v2_n/linalg.norm(v2_n) # Normalizamos o vetor
-		x_robo = [1,0,0]
-		cosa = numpy.dot(n2, x_robo) # Projecao do vetor normal ao marcador no x do robo
-		angulo_marcador_robo = math.degrees(math.acos(cosa))
-
-		# Terminamos
-		print("id: {} x {} y {} z {} angulo {} ".format(id, x,y,z, angulo_marcador_robo))
-
-
+terminator = Terminator()
 
 # A função a seguir é chamada sempre que chega um novo frame
 def roda_todo_frame(imagem):
     print("frame")
-    global cv_image
-    global media
-    global centro
-
-    global resultados
-
-
+    # global terminator.media
+    
     now = rospy.get_rostime()
     imgtime = imagem.header.stamp
-    lag = now-imgtime # calcula o lag
+    lag = now-imgtime  # calcula o lag
     delay = lag.nsecs
     # print("delay ", "{:.3f}".format(delay/1.0E9))
-    if delay > atraso and check_delay==True:
+    if delay > terminator.atraso and terminator.check_delay == True:
         print("Descartando por causa do delay do frame:", delay)
-        return 
+        return
     try:
         antes = time.clock()
-        cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+        terminator.cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
         # Note que os resultados já são guardados automaticamente na variável
         # chamada resultados
-        centro, imagem, resultados =  visao_module.processa(cv_image)        
-        for r in resultados:
+        terminator.centro, terminator.imagem, terminator.resultados = visao_module.processa(terminator.cv_image)
+        for r in terminator.resultados:
             # print(r) - print feito para documentar e entender
-            # o resultado            
+            # o resultado
             pass
 
         depois = time.clock()
@@ -137,20 +73,21 @@ def roda_todo_frame(imagem):
         #cv2.imshow("Camera", cv_image)
     except CvBridgeError as e:
         print('ex', e)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     rospy.init_node("cor")
 
     topico_imagem = "/camera/rgb/image_raw/compressed"
 
-    recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
-    recebedor = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, recebe) # Para recebermos notificacoes de que marcadores foram vistos
-
+    recebedor = rospy.Subscriber(
+        topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size=2**24)
+    # Para recebermos notificacoes de que marcadores foram vistos
+    recebedor = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, terminator.recebe)
 
     print("Usando ", topico_imagem)
 
-    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
-
+    terminator.velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
     tolerancia = 25
 
@@ -160,14 +97,13 @@ if __name__=="__main__":
     try:
         # Inicializando - por default gira no sentido anti-horário
         # vel = Twist(Vector3(0,0,0), Vector3(0,0,math.pi/10.0))
-        
+
         while not rospy.is_shutdown():
-            for r in resultados:
+            for r in terminator.resultados:
                 print(r)
-            #velocidade_saida.publish(vel)
+            # velocidade_saida.publish(vel)
             rospy.sleep(0.1)
 
     except rospy.ROSInterruptException:
         print("Ocorreu uma exceção com o rospy")
-
 
