abcf4618c5af4a56259cda1d44dcb27c0a4426cd
thalials <thalials@al.insper.edu.br>
2020-05-11 15:47:06 -0300

    add cormodule no terminator

--- ros/scripts/TerminatorModule.py
+++ ros/scripts/TerminatorModule.py
@@ -38,7 +38,6 @@ from std_msgs.msg import Header
 import auxiliar as aux
 import visao_module
 
-
 width = "screen width"
 height = "screen height"
 tolerance = 0.01
@@ -47,7 +46,6 @@ direction = {
     "right": Vector3(0, 0, 1),
     "left": Vector3(0, 0, -1)
 }
-
 
 class Terminator():
     def __init__(self):
@@ -163,12 +161,38 @@ class Terminator():
             # corrigir para a direita
             return 0.05
 
-    def procurarCreeper(self):
+    def procurarCreeper(self, distancia):
+        deteccoes = self.identifica_cor()
 
-        pass
+        print("Distancia:", aux.distancia)
+        if len(self.media) != 0 and len(self.centro) != 0:
+            print("Média dos verdes: {0}, {1}".format(self.media[0], self.media[1]))
+            print("Centro dos verdes: {0}, {1}".format(self.centro[0], self.centro[1]))
+           
+            # while not detected:
+            if (self.media[0] > self.centro[0]): #if (media[0] > centro[0] - 5): 
+                # vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.1))
+                self.move(0, -0.1)
+            elif (media[0] < centro[0]): #if (media[0] > centro[0] + 5)
+                # vel = Twist(Vector3(0,0,0), Vector3(0,0,0.1))
+                self.move(0, 0.1)
+
+            if (aux.distancia < .35):
+                # vel = Twist(Vector3(-0.05,0,0), Vector3(0,0,0))
+                self.move(-0.05, 0)
+            else: 
+                # vel = Twist(Vector3(0.05,0,0), Vector3(0,0,0))
+                self.move(0.05, 0)
 
     def alcancarCreeper(self):
-        pass
+        # if aux.distancia < 1.02:
+        #     velocidade = Twist(Vector3(-0.1, 0, 0), Vector3(0, 0, 0))
+        #     self.velocidadeSaida.publish(velocidade)
+        #     rospy.sleep(2)
+        # else:
+        #     velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, 0))
+        #     self.velocidadeSaida.publish(velocidade)
+        #     rospy.sleep(2)
 
     def pegarCreeper(self):
         pass
@@ -362,3 +386,67 @@ class Terminator():
         """Encontra a pista se não estiver nela, se dirige até o
          centro e alinha com a faixa pontilhada central (eu espero);"""
         pass
+
+    def identifica_cor(self):
+    '''
+        Segmenta o maior objeto cuja cor é parecida com cor_h (HUE da cor, no espaço HSV).
+        '''
+
+        # No OpenCV, o canal H vai de 0 até 179, logo cores similares ao
+        # vermelho puro (H=0) estão entre H=-8 e H=8.
+        # Precisamos dividir o inRange em duas partes para fazer a detecção
+        # do vermelho:
+        frame_hsv = cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2HSV)
+
+        cor_menor,cor_maior = aux.ranges([0,99,7]) # devolve dois valores: hsv_menor e hsv_maior
+        segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
+    
+        # Note que a notacão do numpy encara as imagens como matriz, portanto o enderecamento é
+        # linha, coluna ou (y,x)
+        # Por isso na hora de montar a tupla com o centro precisamos inverter, porque
+        centro = (int(self.visionWidth/2), int(self.visionHeight/2))
+
+        # A operação MORPH_CLOSE fecha todos os buracos na máscara menores
+        # que um quadrado 7x7. É muito útil para juntar vários
+        # pequenos contornos muito próximos em um só.
+        segmentado_cor = cv2.morphologyEx(segmentado_cor,cv2.MORPH_CLOSE,np.ones((7, 7)))
+
+        # Encontramos os contornos na máscara e selecionamos o de maior área
+        #contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        maior_contorno = None
+        maior_contorno_area = 0
+
+        for cnt in contornos:
+            area = cv2.contourArea(cnt)
+            if area > maior_contorno_area:
+                maior_contorno = cnt
+                maior_contorno_area = area
+
+        # Encontramos o centro do contorno fazendo a média de todos seus pontos.
+        if not maior_contorno is None :
+            cv2.drawContours(frame, [maior_contorno], -1, [0, 0, 255], 5)
+            maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
+            media = maior_contorno.mean(axis=0)
+            media = media.astype(np.int32)
+            cv2.circle(frame, (media[0], media[1]), 5, [0, 255, 0])
+            cross(frame, centro, [255,0,0], 1, 17)
+        else:
+            media = (0, 0)
+
+        # Representa a area e o centro do maior contorno no frame
+        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        cv2.putText(frame,"{:d} {:d}".format(*media),(20,100), 1, 4,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(frame,"{:0.1f}".format(maior_contorno_area),(20,50), 1, 4,(255,255,255),2,cv2.LINE_AA)
+
+        cv2.imshow('video', frame)
+        cv2.imshow('seg', segmentado_cor)
+        cv2.waitKey(1)
+
+        # self.maior_contorno_area = maior_contorno_area
+        self.media = media
+        self.centro = centro
+
+
+    
--- ros/scripts/base_proj.py
+++ ros/scripts/base_proj.py
@@ -19,7 +19,7 @@ import tf2_ros
 from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
 from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, LaserScan
 from std_msgs.msg import Header
 from numpy import linalg
 from tf import transformations
@@ -41,11 +41,8 @@ import visao_module
 # módulo de controle da garra: roslaunch turtlebot3_manipulation_moveit_config move_group.launch
 # GUI de interação com a garra: roslaunch turtlebot3_manipulation_gui turtlebot3_manipulation_gui.launch
 
-
 cv_image = None
 atraso = 1.5E9  # 1 segundo e meio. Em nanossegundos
-
-
 
 # A função a seguir é chamada sempre que chega um novo frame
 
