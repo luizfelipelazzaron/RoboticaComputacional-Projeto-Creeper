8d26b524e0b9845009c39be7a4229a8778d3f599
luizfelipelazzaron <luizfl@al.insper.edu.br>
2020-05-11 15:54:04 -0300

    atualizacao

--- ros/scripts/TerminatorModule.py
+++ ros/scripts/TerminatorModule.py
@@ -59,6 +59,8 @@ class Terminator():
         self.tolerance = 20
         self.estacao = None
         self.image = None
+        self.counter = 0
+        self.counterLimit = 5
         # retirados de base_proj.py
         self.cvImage = None
         self.visionHeight = None
@@ -127,80 +129,45 @@ class Terminator():
     def iniciar(self):
         self.task['iniciar'] = False
         self.task['percorrerPista'] = True
+        self.task['procurarCreeper'] = True
 
     def procurarPista(self):
-        # Moveria para frente
-        # Se ele identificar algum obstáculo , ele começa a rotacionar
-        # Se ele identificar a pista, e encerra a task
-        # self.move(0.1, 0)
-
-        pass
+        if self.counter < self.counterLimit:
+            frame = self.cvImage
+            frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            cor_menor,cor_maior = aux.ranges([62,100,100]) # amarelo
+            centro = (frame.shape[1]//2, frame.shape[0]//2)
+        else:
+            print("mudando de estado")
+            self.task['procurarPista'] = False
+            self.task['percorrerPista'] = True
+            self.counter = 0 
 
     def alcancarPista(self):
         pass
 
-    # def percorrerPista(self, image):
-    #     while(True):
-    #         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)                      # Conversão para escala de cinza
-    #         gray = cv2.GaussianBlur(gray, (5, 5), 0.33)                         # Redução de ruído 1# Redução de ruído 1
-    #         limiar, img_limiar = cv2.threshold(                                 # limiarização da imagem ( fonte: https://youtu.be/P2R7Nn1_VwQ )
-    #             gray, 180, 255, cv2.THRESH_BINARY)
-    #         KERNEL = np.array([                                                 # Redução de ruido 2
-    #             [0, 0, 1, 0, 0],                                                # Aplicação de um kernel personalizado ( fonte: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html)
-    #             [0, 0, 1, 0, 0],
-    #             [0, 1, 1, 1, 0],
-    #             [0, 1, 1, 1, 0],
-    #             [0, 1, 1, 1, 0],
-    #             [0, 0, 1, 0, 0],
-    #             [0, 0, 1, 0, 0]
-    #         ], dtype="uint8")
-    #         segmentado = cv2.morphologyEx(img_limiar, cv2.MORPH_OPEN, KERNEL)
-    #         segmentado = cv2.morphologyEx(segmentado, cv2.MORPH_OPEN, KERNEL)
-    #         segmentado = cv2.morphologyEx(segmentado, cv2.MORPH_OPEN, KERNEL)
-    #         segmentado = cv2.morphologyEx(segmentado, cv2.MORPH_OPEN, KERNEL)
-    #         segmentado = cv2.morphologyEx(segmentado, cv2.MORPH_CLOSE, KERNEL)
-    #         segmentado = cv2.morphologyEx(segmentado, cv2.MORPH_CLOSE, KERNEL)
-    #         segmentado = cv2.morphologyEx(segmentado, cv2.MORPH_CLOSE, KERNEL)
-    #         segmentado = cv2.morphologyEx(segmentado, cv2.MORPH_CLOSE, KERNEL)
-    #         blur = cv2.GaussianBlur(segmentado, (11, 11), 0.33)                 # Redução de ruído 3
-    #         shapes = cv2.Canny(blur, 50, 200)                                   # Detecção de contornos
-    #         lines = cv2.HoughLinesP(                                            # A partir dos contornos, gera um array com linhas que satisfazem os argumentos
-    #             shapes, 1, pi/180, 50, minLineLength=100, maxLineGap=20)        # minLineLength e maxLineGap (parâmetros foram modificados até que um resultado satisfatório fosse atingido)
-    #         print(lines[0])
-    #         OUTPUT = image
-    #         red_lines = []
-    #         blue_lines = []
-    #         try:
-    #             m1, n1 = coeficientes(red_lines)                                # função 'coeficientes' definida lá em cima
-    #             m2, n2 = coeficientes(blue_lines)                               # m e n são conficientes de retas da forma:  y = mx + n
-    #         except:
-    #             pass
-    #         finally:
-    #             X = max(0, min(int((n2-n1)/(m1-m2)), WIDTH))                    # min() e max() servem para deixar o circulo sempre visível
-    #             Y = max(0, min(int((m1*X + n1)), HEIGHT))                       # X e Y são as coordenadas do ponto de encontro entre a reta média azul e a reta média vermelha
-    #             cv2.circle(OUTPUT, (X, Y), 10, (0, 255, 0), 2, 2)               # print("(X,Y) =",(X,Y)) # descomente essa linha para printar no terminal as coordenadas do centro
-    #         cv2.putText(OUTPUT," Aperte q para sair", (0,50), font, 1,(255,255,255),2,cv2.LINE_AA) # Adicionar textos na tela
-    #         cv2.imshow("output", OUTPUT)                                        # Exibir o resultado
-    #         if cv2.waitKey(1) & 0xFF == ord('q'):
-    #             break
-        # try:
-        #     self.task['percorrerPista'] = False
-        #     self.task['procurarCreeper'] = True
-        # except:
-        #     pass
     def percorrerPista(self):
-        localTarget = self.followPath()
+        if self.counter < self.counterLimit:
+            try:
+                localTarget = self.followPath()
 
-        if self.targetInCenter(localTarget):
-            print("linha reta")
-            self.move(0.5, 0)
+                if self.targetInCenter(localTarget):
+                    print("linha reta")
+                    self.move(0.5, 0)
+                else:
+                    # self.stop()
+                    self.move(0.08, self.whereTo(localTarget[0]))
+                cv2.circle(self.cvImage, (localTarget[0], localTarget[1]), 10, (0, 255, 0), 2, 2)
+                cv2.imshow("Terminator Vision", self.cvImage)
+                cv2.waitKey(1)
+            except:
+                self.counter += 1
+                print("contador: ", self.counter)
         else:
-            # self.stop()
-            self.move(0.08, self.whereTo(localTarget[0]))
-        
-        cv2.circle(self.cvImage, (localTarget[0], localTarget[1]), 10, (0, 255, 0), 2, 2)
-        cv2.imshow("Terminator Vision", self.cvImage)
-        cv2.waitKey(1)
+            print("Deu certo")
+            self.task['percorrerPista'] = False
+            self.task['procurarPista'] = True
+            self.counter = 0
  
     def whereTo(self, x):
         if x > self.visionWidth/2:
@@ -213,7 +180,6 @@ class Terminator():
             return 0.05
 
     def procurarCreeper(self):
-
         pass
 
     def alcancarCreeper(self):
