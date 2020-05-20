5c7cfa9d59a693232272d6954b372251635bd0d7
borg <elm.tiago@gmail.com>
2020-05-20 00:08:35 -0300

    alterações do Ciccero

--- ros/scripts/TerminatorModule.py
+++ ros/scripts/TerminatorModule.py
@@ -130,7 +130,6 @@ class Terminator():
                 self.procurarEstacao()
             elif self.task['alcancarEstacao']:
                 self.alcancarEstacao()
-            # self.imShow()
         else:
             pass
 
@@ -394,35 +393,35 @@ class Terminator():
                 right = list(filter(lambda line: 0.3 <
                                     aux.tangente(line) < 19, lines))
             except:
-                print("caiu no except")
                 pass
         # agora que temos listas de linhas com inclinaçao
         # negativa (esquerda) e positiva (direita), transformamos em array
         # para executar umas operações mais sucintas
-        try:
+        if left:
             left = np.array(left)
             for leftLine in left:
                 aux.draw_line(frame, leftLine, color=(32, 0, 255))
-        except:
-            return 0, (self.visionHeight/2)
-        try:
+        
+        if right:
             right = np.array(right)
             for rightLine in right:
                 aux.draw_line(frame, rightLine, color=(255, 32, 0))
-        except:
-            return int(self.visionWidth), (self.visionHeight/2)
-
+        
         try:
             # reta média esquerda
             m1, n1 = aux.coefficients(left)
             # print("m1,n1:", m1, n1)
         except:
+            cv2.circle(frame, (0, int(self.visionHeight/2)), 10, (0, 255, 0), 2, 2)
+            self.finalImage = frame
             return 0, int(self.visionHeight/2)
         try:
             # reta média direita
             m2, n2 = aux.coefficients(right)
             # print("m2,n2", m2, n2)
         except:
+            cv2.circle(frame, (int(self.visionWidth), int(self.visionHeight/2)), 10, (0, 255, 0), 2, 2)
+            self.finalImage = frame
             return int(self.visionWidth), int(self.visionHeight/2)
         # min() e max() servem para deixar o circulo sempre visível
         # X e Y são as coordenadas do ponto de encontro entre a reta média azul e a reta média vermelha
@@ -432,15 +431,10 @@ class Terminator():
         Y = int(self.visionHeight/2)
         # print("(X,Y) =",(X,Y)) # descomente essa linha para printar no terminal
         # as coordenadas do centro
-
         cv2.circle(frame, (X, Y), 10, (0, 255, 0), 2, 2)
         self.finalImage = frame
         return X, Y
 
-    def pathFinder(self):
-        """Encontra a pista se não estiver nela, se dirige até o
-         centro e alinha com a faixa pontilhada central (eu espero);"""
-        pass
 
     def identifica_cor(self, colorRgb):
         '''
